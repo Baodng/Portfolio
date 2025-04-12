bb5c8480a1895d2e81648de85e47c8a8dd60ad36
Gia Bao <baodngse184091@fpt.edu.vn>
2025-04-13 02:09:32 +0700

    fixx

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Portfolio/',
+  base: './',
   plugins: [
     react(), // ✅ Chỉ cần thế này là đủ cho "use client"
   ],
