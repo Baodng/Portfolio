0a1ed183d1c7e1fab2d2ba39824c40d51f26fe63
Gia Bao <baodngse184091@fpt.edu.vn>
2025-04-13 02:07:40 +0700

    fix 404 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  base: '<Portfolio>/', // VD: '/my-app/'
+  base: '/Portfolio/',
   plugins: [
-    tailwindcss(),
+    react(), // ✅ Chỉ cần thế này là đủ cho "use client"
   ],
 })
